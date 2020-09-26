406494a6ce732e1b0e3c9cdea1284e93f1d84ed0
Gustavo Silva <chdrlz@gmail.com>
2020-09-25 21:02:00 -0300

    Connect with MongoDB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+mongoose.connect(
+  'mongodb+srv://gustavo:123@cluster0.yasa4.mongodb.net/dbname?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ Hello: 'World' });
-});
+app.use(routes);
 
 app.listen(3333);
