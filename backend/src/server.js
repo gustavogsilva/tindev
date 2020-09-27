df5fe33148e52da5cbe5cdaf5429935d31e16567
Gustavo Silva <chdrlz@gmail.com>
2020-09-27 17:55:31 -0300

    Adds socket.io and .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const routes = require('./routes');
 
-mongoose.connect(
-  'mongodb+srv://gustavo:123@cluster0.yasa4.mongodb.net/dbname?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
+dotenv.config();
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(process.env.PORT, () => {
+  console.log(`app is running on ${process.env.HOST}:${process.env.PORT}`);
+});
