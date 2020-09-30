c225212633fe2ff336787fbee09c6c1a2e838b9a
Gustavo Silva <chdrlz@gmail.com>
2020-09-30 01:06:52 -0300

    Deploy on heroku

---
+++ src/client/build/precache-manifest.c1524150befb10d437d7e153accc6d0b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b820c9039f9fe43444dfb2566ef107a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "231b85bfba86157361c6",
+    "url": "/static/css/main.35dc7e36.chunk.css"
+  },
+  {
+    "revision": "8d0fe234bbf192c7d5f4",
+    "url": "/static/js/2.fb1ccbc7.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.fb1ccbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231b85bfba86157361c6",
+    "url": "/static/js/main.466f1a1d.chunk.js"
+  },
+  {
+    "revision": "64dc74e3c79e8383d689",
+    "url": "/static/js/runtime-main.f03cf26b.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "21c10b2613fbc1b8b36859e433819ed1",
+    "url": "/static/media/itsamatch.21c10b26.png"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/static/media/logo.6a01ceef.svg"
+  }
+]);
